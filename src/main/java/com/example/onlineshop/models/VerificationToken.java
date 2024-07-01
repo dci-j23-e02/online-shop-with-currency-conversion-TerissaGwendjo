f4c074e355d5d8c4a3b3a4c53b3f4dea4c7d1860
dci-student <Terissa>
2024-07-01 09:51:08 +0200

    Complete verificationToken entity class implementation
    Add fields for user attributes, include annotations for ORM mapping, ensure all necessary getters and setters are included.

---
+++ src/main/java/com/example/onlineshop/models/VerificationToken.java
@@ -0,0 +1,68 @@
+package com.example.onlineshop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "verification-tokens")
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    //Constructors
+    public VerificationToken() {
+    }
+    public VerificationToken(String token, User user, LocalDateTime expiryDate) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+    public VerificationToken(Long id, String token, User user, LocalDateTime expiryDate) {
+        this.id = id;
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+
+    //Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
