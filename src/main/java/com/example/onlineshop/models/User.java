ee994a6e50772bbbd8b8933fc55246337a2d7d69
dci-student <Terissa>
2024-07-01 09:01:17 +0200

    online shop application

--- src/main/java/com/example/onlineshop/models/User.java
+++ src/main/java/com/example/onlineshop/models/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import jakarta.persistence.Table;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -19,4 +22,13 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
