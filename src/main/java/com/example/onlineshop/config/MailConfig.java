7491678fb9a8ebaef0f46dc21e8053d581c0b6ba
dci-student <Terissa>
2024-07-01 11:48:26 +0200

    password encoder configuration and mail configuration completed

---
+++ src/main/java/com/example/onlineshop/config/MailConfig.java
@@ -0,0 +1,29 @@
+package com.example.onlineshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("gweter4@gmail.com");
+        mailSender.setPassword("fatu hauh xkas weaq");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
