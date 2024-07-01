d587dae60f59400435c14353e5d799aa7ffa77b9
dci-student <Terissa>
2024-07-01 08:50:44 +0200

    online shop application

---
+++ src/main/java/com/example/onlineshop/OnlineShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Annotates this class as a Spring Boot application
+public class OnlineShopApplication {
+    public static void main(String[] args) {
+        // Launches the Spring Boot application
+        SpringApplication.run(OnlineShopApplication.class, args);
+    }
+}
